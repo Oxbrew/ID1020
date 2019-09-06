493dc2c75e1ea33623c2398f2f0ad2d7c5c042a3
Oxbrew <{13368707}+{Oxbrew}@users.noreply.github.com>
2019-09-06 06:42:31 +0200

    edit

--- Lab1/crecursive.c
+++ Lab1/crecursive.c
@@ -12,6 +12,15 @@ int main() {
 	return 0;
 }
 
+/* NOTE: by using getchar() we are able to use the same variable name
+* 'c' without it being overwritten. This is because when getchar is called it
+* moves forward in memory by (int) bits (8)
+*
+* putchar() allows us to do the reverse whilst printing it.
+*
+* The same works with scanf and printf stdin and stout functions.;
+*/
+
 void recursivefunct() {
 	char c;
 	c = getchar();
@@ -23,4 +32,4 @@ void recursivefunct() {
 		//printf("%c\n", c );
 	}
 	printf("\n");
-}
+}a
