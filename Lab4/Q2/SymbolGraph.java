9bb1be35c40116882136bec83195cb19f1278ba7
Oxbrew <13368707+Oxbrew@users.noreply.github.com>
2019-10-04 09:17:45 +0200

    progress

--- Lab4/Q2/SymbolGraph.java
+++ Lab4/Q2/SymbolGraph.java
@@ -51,45 +51,49 @@ public class SymbolGraph {
 
 	/* BELOW IS THE DEPTH FIRST SEARCH ALGORITHM*/
 
-	public static class DFS {
-		private boolean[] marked; // Has dfs() been called for this vertex?
+	public static class BFS {
+		private boolean[] marked; // Is a shortest path to this vertex known?
 		private int[] edgeTo; // last vertex on known path to this vertex
-		private final int s; // source
-		public DFS(Graph G, int s) {
+		private final int s;
+		// source
+		public BFS(Graph G, int s) {
 			marked = new boolean[G.V()];
 			edgeTo = new int[G.V()];
 			this.s = s;
-			dfs(G, s);
+			bfs(G, s);
+		}
+		private void bfs(Graph G, int s) {
+			LinkedList<Integer> queue = new LinkedList<Integer>();
+			marked[s] = true;// Mark the source
+			queue.add(s); //and put it on the queue.
+			while (!queue.isEmpty()) {
+				int v = queue.remove(); // Remove next vertex from the queue.
+				for (int w : G.adj(v))
+				if (!marked[w]){ // For every unmarked adjacent vertex,
+					edgeTo[w] = v; // save last edge on a shortest path,
+					marked[w] = true; //mark it because path is known,
+					queue.add(w); //and add it to the queue.
+				}
 			}
+		}
+		public boolean hasPathTo(int v)
+		{ return marked[v]; }
 
-		private void dfs(Graph G, int v) {
-			marked[v] = true;
-			for (int w : G.adj(v))
-			if (!marked[w]) {
-				edgeTo[w] = v;
-				dfs(G, w);
+			public Iterable<Integer> pathTo(int v) {
+				if (!hasPathTo(v))
+					return null;
+				LinkedList<Integer> path = new LinkedList<Integer>();
+				for (int x = v ; x != s ; x = edgeTo[x])
+					path.addFirst(x);
+				path.addFirst(s);
+
+
+				/*Stack<Integer> path = new Stack<Integer>();
+				for (int x = v; x != s; x = edgeTo[x])
+				path.push(x);
+				path.push(s);*/
+				return path;
 			}
-		}
-
-		public boolean hasPathTo(int v) {
-			return marked[v];
-		}
-
-		public Iterable<Integer> pathTo(int v) {
-			if (!hasPathTo(v))
-				return null;
-			LinkedList<Integer> path = new LinkedList<Integer>();
-			for (int x = v ; x != s ; x = edgeTo[x])
-				path.addFirst(x);
-			path.addFirst(s);
-
-
-			/*Stack<Integer> path = new Stack<Integer>();
-			for (int x = v; x != s; x = edgeTo[x])
-			path.push(x);
-			path.push(s);*/
-			return path;
-		}
 	}
 
 	public static void main(String[] args) {
@@ -106,7 +110,7 @@ public class SymbolGraph {
 		String from = args[0];
 		String to = args[1];
 
-		DFS search = new DFS(SG.G(), SG.index(from));
+		BFS search = new BFS(SG.G(), SG.index(from));
 
 		System.out.println(from + " -> " + to + ": ");
 		if (search.hasPathTo(SG.index(to))) {
